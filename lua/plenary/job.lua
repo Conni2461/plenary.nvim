692935ee3031929f41486d453d3eee4e48753cad
TJ DeVries <devries.timothyj@gmail.com>
2020-08-31 19:39:20 -0400

    feat: Schedule logging for later

---
+++ lua/plenary/job.lua
@@ -0,0 +1,348 @@
+local vim = vim
+local uv = vim.loop
+
+local Job = {}
+Job.__index = Job
+
+local function close_safely(handle)
+  if not handle then
+    return
+  end
+
+  if not handle:is_closing() then
+    handle:close()
+  end
+end
+
+local shutdown_factory = function (child)
+  return function(code, signal)
+    child:shutdown(code, signal)
+  end
+end
+
+
+--- Create a new job
+--@param o table
+--@field o.command string  : Command to run
+--@field o.args Array      : List of arguments to pass
+--@field o.cwd string      ? Working directory for job
+--@field o.env Map         : Environment
+--@field o.detach callable : Function to call on detach.
+--
+--@field o.call_on_lines boolean? Only call callbacks on line.
+function Job:new(o)
+  local obj = {}
+
+  obj.command = o.command
+  obj.args = o.args
+  obj.cwd = o.cwd
+  obj.env = o.env
+  obj.detach = o.detach
+
+  obj._user_on_start = o.on_start
+  obj._user_on_stdout = o.on_stdout
+  obj._user_on_stderr = o.on_stderr
+  obj._user_on_exit = o.on_exit
+
+  obj._additional_on_exit_callbacks = {}
+
+  obj._maximum_results = o.maximum_results
+
+  obj.user_data = {}
+
+  obj.writer = o.writer
+
+  self._reset(obj)
+
+  return setmetatable(obj, self)
+end
+
+function Job:_reset()
+  self.is_shutdown = nil
+  self.results = nil
+end
+
+--- Stop a job and close all handles
+function Job:_stop()
+  close_safely(self.stdin)
+  close_safely(self.stderr)
+  close_safely(self.stdout)
+  close_safely(self.handle)
+end
+
+--- Shutdown a job.
+function Job:shutdown(code, signal)
+  self.code = code
+  self.signal = signal
+
+  if self._user_on_exit then
+    self:_user_on_exit(code, signal)
+  end
+
+  for _, v in ipairs(self._additional_on_exit_callbacks) do
+    v(self, code, signal)
+  end
+
+  self.stdout:read_stop()
+  self.stderr:read_stop()
+
+  self:_stop()
+
+  self.is_shutdown = true
+end
+
+function Job:_create_uv_options()
+  local options = {}
+
+  options.command = self.command
+  options.args = self.args
+  options.stdio = {
+    self.stdin,
+    self.stdout,
+    self.stderr
+  }
+
+  if self.cwd then
+    options.cwd = self.cwd
+  end
+
+  if self.env then
+    options.env = self.env
+  end
+
+  if self.detach then
+    options.detach = self.detach
+  end
+
+  return options
+end
+
+-- TODO: Add the ability to have callback called ONLY on complete lines.
+--          Remember, to send the last line when you're done though :laugh:
+local on_output = function(self, cb)
+  if not self.results then
+    self.results = {''}
+  end
+
+  local results = self.results
+
+  return function(err, data)
+    if data == nil then
+      if results[#results] == '' then
+        table.remove(results, #results)
+      end
+
+      return
+    end
+
+    local line, start, found_newline
+    repeat
+      start = string.find(data, "\n") or #data
+      found_newline = string.find(data, "\n")
+
+      line = string.sub(data, 1, start - 1)
+      data = string.sub(data, start + 1, -1)
+
+      line = line:gsub("\r", "")
+
+      results[#results] = (results[#results] or '') .. line
+
+      if found_newline then
+        local result_number = #results
+
+        if cb then
+          cb(err, results[result_number], self)
+        end
+
+        -- Stop processing if we've surpassed the maximum.
+        if self._maximum_results then
+          if result_number > self._maximum_results then
+            self:shutdown()
+            return
+          end
+        end
+
+        table.insert(results, '')
+      end
+    until not found_newline
+
+  end
+end
+
+--- Stop previous execution and add new pipes.
+--- Also regenerates pipes of writer.
+function Job:_prepare_pipes()
+  self:_stop()
+
+  if self.writer then
+    self.writer:_prepare_pipes()
+  end
+
+  self.stdin = (self.writer and self.writer.stdout) or uv.new_pipe(false)
+  self.stdout = uv.new_pipe(false)
+  self.stderr = uv.new_pipe(false)
+end
+
+--- Execute job. Should be called only after preprocessing is done.
+function Job:_execute()
+  self:_reset()
+
+  local options = self:_create_uv_options()
+
+  if self._user_on_start then
+    self:_user_on_start()
+  end
+
+  self.handle, self.pid = uv.spawn(
+    options.command,
+    options,
+    vim.schedule_wrap(shutdown_factory(self))
+  )
+
+  self.stdout:read_start(on_output(self, self._user_on_stdout))
+  self.stderr:read_start(on_output(self, self._user_on_stderr))
+
+  if self.writer then
+    self.writer:_execute()
+  end
+
+  return self
+end
+
+function Job:start()
+  self:_prepare_pipes()
+  self:_execute()
+end
+
+function Job:sync()
+  self:start()
+  self:wait()
+
+  return self:result()
+end
+
+function Job:result()
+  return self.results
+end
+
+function Job:pid()
+  return self.pid
+end
+
+function Job:wait()
+  if self.handle == nil then
+    vim.api.nvim_err_writeln(vim.inspect(self))
+    return
+  end
+
+  while
+    not vim.wait(
+      100,
+      function() 
+        if self.is_shutdown then
+          assert(self.handle:is_closing(), "Job must be shutdown if it's closing")
+        end
+
+        return self.is_shutdown
+      end,
+      10)
+  do
+  end
+
+  return self
+end
+
+function Job:co_wait(wait_time)
+  wait_time = wait_time or 5
+
+  if self.handle == nil then
+    vim.api.nvim_err_writeln(vim.inspect(self))
+    return
+  end
+
+  while not vim.wait(wait_time, function() return self.is_shutdown end) do
+    coroutine.yield()
+  end
+
+  return self
+end
+
+--- Wait for all jobs to complete
+function Job.join(...)
+  local jobs_to_wait = {...}
+
+  while true do
+    if #jobs_to_wait == 0 then
+      break
+    end
+
+    local current_job = jobs_to_wait[1]
+    if current_job.is_shutdown then
+      table.remove(jobs_to_wait, 1)
+    end
+
+    -- vim.cmd.sleep(10)
+    vim.cmd("sleep 100m")
+  end
+end
+
+local _request_id = 0
+local _request_status = {}
+
+function Job.chain(...)
+  _request_id = _request_id + 1
+  _request_status[_request_id] = false
+
+  local jobs = {...}
+
+  for index, job in ipairs(jobs) do
+    if index ~= 1 then
+      local prev_job = jobs[index - 1]
+      local original_on_exit = prev_job._user_on_exit
+      prev_job._user_on_exit = function(self, err, data)
+        if original_on_exit then
+          original_on_exit(self, err, data)
+        end
+
+        job:start()
+      end
+    end
+  end
+
+  local last_on_exit = jobs[#jobs]._user_on_exit
+  jobs[#jobs]._user_on_exit = function(self, err, data)
+    if last_on_exit then
+      last_on_exit(self, err, data)
+    end
+
+    _request_status[_request_id] = true
+  end
+
+  jobs[1]:start()
+
+  return _request_id
+end
+
+function Job.chain_status(id)
+  return _request_status[id]
+end
+
+function Job.is_job(item)
+  if type(item) ~= 'table' then
+    return false
+  end
+
+  return getmetatable(item) == Job
+end
+
+function Job:add_on_exit_callback(cb)
+  table.insert(self._additional_on_exit_callbacks, cb)
+end
+
+--- Send data to a job.
+function Job:send(data)
+  self.stdin:write(data)
+end
+
+
+return Job
---
+++ lua/plenary/test_harness.lua
@@ -0,0 +1,202 @@
+local lu = require("luaunit")
+
+local Path = require("plenary.path")
+local Job = require("plenary.job")
+
+local f = require("plenary.functional")
+local log = require("plenary.log")
+local win_float = require("plenary.window.float")
+
+local headless = require("plenary.nvim_meta").is_headless
+
+local harness = {}
+
+local function validate_test_type(test_type)
+  if test_type ~= 'luaunit' and test_type ~= 'busted' then
+    error(
+      string.format(
+        'Unexpected test_type: %s. Expected luaunit or busted.\n%s',
+        test_type,
+        debug.traceback()
+      )
+    )
+  end
+end
+
+local print_output = function(_, ...)
+  for _, v in ipairs({...}) do
+    print(v)
+  end
+end
+
+local nvim_output = function(bufnr, ...)
+  vim.fn.nvim_buf_set_lines(bufnr, -1, -1, false, {...})
+end
+
+function harness:run(test_type, bufnr, win_id, ...)
+  validate_test_type(test_type)
+
+  if bufnr == nil then
+    bufnr = vim.fn.nvim_create_buf(false, true)
+  end
+
+  if win_id == nil then
+    -- TODO: Could just make win be 0...?
+    local opts = win_float.default_opts()
+    win_id = vim.fn.nvim_open_win(bufnr, true, opts)
+  end
+
+  vim.fn.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
+  vim.fn.nvim_buf_set_option(bufnr, 'bufhidden', 'hide')
+  vim.fn.nvim_buf_set_option(bufnr, 'swapfile', false)
+
+  if test_type == 'luaunit' then
+    print("\n")
+    print("===== Results ===== ")
+    print("\n")
+    print("\n")
+
+    local luaunit_result = lu.LuaUnit.run(...)
+    if headless and luaunit_result ~= 0 then
+      os.exit(luaunit_result)
+    end
+
+  elseif test_type == 'busted' then
+    -- Requires people to have called `setup_busted`
+  else
+    assert(false)
+  end
+
+  -- Would not mind having a bit nicer output, but it's fine for now.
+  -- lu.LuaUnit.run("--outputtype=tap")
+
+  -- weirdly need to redraw the screen sometimes... oh well
+  vim.fn.win_gotoid(win_id)
+  vim.cmd("mode")
+  vim.cmd("nnoremap q :q<CR>")
+end
+
+function harness:test_directory(test_type, directory)
+  validate_test_type(test_type)
+
+  log.debug("Starting...")
+  if test_type == 'busted' then
+    -- Only need to make sure penlight/lfs is available, since we have slightly different busted
+    -- require('plenary.neorocks').ensure_installed('luafilesystem', 'lfs', true)
+    -- require('plenary.neorocks').ensure_installed('penlight', 'pl', true)
+  end
+
+
+  local res = win_float.centered()
+  vim.cmd('mode')
+  vim.fn.nvim_buf_set_keymap(res.bufnr, "n", "q", ":q<CR>", {})
+
+  local outputter
+  if headless then
+    outputter = print_output
+  else
+    outputter = nvim_output
+  end
+
+  local paths = self:_find_files_to_run(directory)
+  local jobs = f.map(
+    function(p)
+      return Job:new({
+        command = 'nvim',
+        args = {
+          '--headless',
+          '-c',
+          string.format(
+            'lua require("plenary.test_harness"):_run_path("%s", "%s")',
+            test_type, p
+          )
+        },
+        -- Can be turned on to debug
+        on_stdout = function(...)
+          if p_debug then
+            print("STDOUT:", ...)
+          end
+        end,
+        on_stderr = function(...)
+          if p_debug then
+            print("STDERR:", ...)
+          end
+        end,
+        on_exit = function(j_self, _, _)
+          outputter(res.bufnr, unpack(j_self:result()))
+          vim.cmd('mode')
+        end
+      })
+    end,
+    paths
+  )
+
+  log.debug("Running...")
+  for _, j in ipairs(jobs) do
+    j:start()
+  end
+
+  log.debug("...Waiting")
+  Job.join(unpack(jobs))
+  log.debug("Done...")
+
+  if headless then
+    if f.any(function(_, v) return v.code ~= 0 end, jobs) then
+      os.exit(1)
+    end
+
+    vim.cmd('qa!')
+  end
+end
+
+function harness:_find_files_to_run(directory)
+  local finder = Job:new({
+    command = 'find',
+    args = {directory, '-type', 'f', '-name', '*_spec.lua'},
+  })
+
+  return f.map(Path.new, finder:sync())
+end
+
+function harness:_run_path(test_type, directory)
+  validate_test_type(test_type)
+
+  local paths = harness:_find_files_to_run(directory)
+
+  local bufnr = 0
+  local win_id = 0
+
+  for _, p in pairs(paths) do
+    print(" ")
+    print("Loading Tests For: ", p:absolute(), "\n")
+
+    dofile(p:absolute())
+    -- local ok, _ = pcall(function() dofile(p:absolute()) end)
+
+    -- if not ok then
+    --   print("Failed to load file")
+    -- end
+  end
+
+  harness:run(test_type, bufnr, win_id)
+  vim.cmd("qa!")
+
+  return paths
+end
+
+
+function harness:setup_busted()
+  if not pcall(require, 'lfs') then
+    vim.api.nvim_err_writeln("Lua Filesystem (lfs) is required.")
+    return
+  end
+
+  if not pcall(require, 'pl') then
+    vim.api.nvim_err_writeln("Penlight (pl) is required.")
+    return
+  end
+
+  require('busted.runner')({output='plainTerminal'}, 3)
+end
+
+return harness
